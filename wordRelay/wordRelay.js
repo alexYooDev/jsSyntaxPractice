735c645630607eff6bad8fa745ea2b026e1801da
fm247 <hiyoo95@gmail.com>
2021-07-18 17:19:32 +0900

    code completed - needs refactoring

--- wordRelay/wordRelay.js
+++ wordRelay/wordRelay.js
@@ -1,10 +1,9 @@
-// function chkFirstPlayer(word) {
-//   if (word.target.value === null) {
-//     return true;
-//   } else {
-//     return false;
-//   }
-// }
+function chkFirstPlayer(word) {
+  word = newWord;
+  $word.textContent = word;
+  $input.value = '';
+  $input.focus();
+}
 
 // 첫번째 단어
 let word;
@@ -12,11 +11,11 @@ let word;
 let newWord;
 let order;
 
+const number = Number(prompt('몇 명이 참가하나요?'));
 const $input = document.querySelector('input');
 const $button = document.querySelector('button');
 const $word = document.querySelector('#word');
 const $order = document.querySelector('#order');
-const number = Number(prompt('몇 명이 참가하나요?'));
 //callback function = 어떤 기능을 수행한 다음 연이어 실행되는 함수.
 //callback function이 여러번 재사용 될 경우, 변수로 내어 저장하는 것이 좋음.
 // 코드를 작성할 때는 절차적으로 순서도를 그려 컴퓨테이셔널 사고력을 기르는 것이 중요.
@@ -26,6 +25,8 @@ function onClick() {
     // 데이터를 바꾸면 화면도 바꾸어 주어야 함 (이 두가지는 항상 같이 따라와야 함)
     word = newWord;
     $word.textContent = word;
+    $input.value = '';
+    $input.focus();
     let order = parseInt($order.textContent); // 현재 순서
     if (order + 1 > number) {
       $order.textContent = 1;
@@ -36,16 +37,30 @@ function onClick() {
   } else {
     // 제시어의 마지막 인덱스 값이 새로운 단어의 첫글짜가 같냐?
     if (word[word.length - 1] === newWord[0]) {
-      // 그렇다면 새로운 단어를 제시어로 변경
+      // 맞는 단어: 그렇다면 새로운 단어를 제시어로 변경
       word = newWord;
       $word.textContent = word;
       $input.value = '';
+      $input.focus();
+      let order = parseInt($order.textContent); // 현재 순서
+      if (order + 1 > number) {
+        $order.textContent = 1;
+      } else {
+        $order.textContent = order + 1;
+      }
     } else {
+      // 틀린 단어: 오답 메시지
       alert('Wrong answer!');
       word = '';
       $word.textContent = word;
       $input.value = '';
-      order = 1;
+      $input.focus();
+      let order = parseInt($order.textContent); // 현재 순서
+      if (order + 1 > number) {
+        $order.textContent = 1;
+      } else {
+        $order.textContent = order + 1;
+      }
     }
   }
 }
