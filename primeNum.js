70f5cfd14605eb77833d09966cf27838f285ea82
fm247 <hiyoo95@gmail.com>
2021-08-09 23:10:31 +0900

    implement permutation

---
+++ primeNum.js
@@ -0,0 +1,30 @@
+function solution(numbers) {
+  let answer = 0;
+  let arr = numbers.split('');
+  console.log(permute(arr));
+}
+
+function permute(permutation) {
+  let length = permutation.length,
+    result = [permutation.slice()],
+    c = new Array(length).fill(0),
+    i = 1,
+    k,
+    p;
+  while (i < length) {
+    if (c[i] < i) {
+      p = permutation[i];
+      permutation[i] = permutation[k];
+      permutation[k] = p;
+      ++c[i];
+      i = 1;
+      result.push(permutation.slice());
+    } else {
+      c[i] = 0;
+      ++i;
+    }
+    return result;
+  }
+}
+
+console.log(solution('17'));
