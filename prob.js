2ea9a0d13f78f137e8146262bda0f93287281e3f
fm247 <hiyoo95@gmail.com>
2021-08-15 01:21:17 +0900

    Weekly Coding Challenge: Mutual Score Assessement

--- prob.js
+++ prob.js
@@ -1,8 +1,17 @@
 function solution(scores) {
   let answer = '';
   let arr = scores;
-  let total = 0;
+  let total = [];
   let avg = [];
+  let a_len = [];
+
+  const transpose = (matrix) =>
+    matrix.reduce(
+      (result, row) => row.map((_, i) => [...(result[i] || []), row[i]]),
+      []
+    );
+
+  arr = transpose(arr);
 
   for (let i = 0; i < arr.length; i++) {
     for (let j = 0; j < arr.length; j++) {
@@ -10,14 +19,53 @@ function solution(scores) {
         (i === j && arr[i][j] === Math.max(...arr[i])) ||
         (i === j && arr[i][j] === Math.min(...arr[i]))
       ) {
-        arr[i].splice(j, 1);
+        arr[i].splice(i, 1);
       }
     }
-    total += arr[i][j];
+    a_len.push(arr[i].length);
+  }
+  console.log(arr);
+  console.log(a_len);
+
+  // const sumArr = (arr) => {
+  //   const newArr = [];
+  //   arr.forEach((sub) => {
+  //     sub.forEach((num, idx) => {
+  //       if (newArr[idx]) {
+  //         newArr[idx] += num;
+  //       } else {
+  //         newArr[idx] = num;
+  //       }
+  //     });
+  //   });
+  //   return newArr;
+  // };
+
+  total = arr.map(function (arr) {
+    return arr.reduce(function (sum, item) {
+      return sum + item;
+    });
+  });
+
+  for (let k = 0; k < a_len.length; k++) {
+    avg.push(total.shift() / a_len[k]);
   }
 
+  for (let l = 0; l < avg.length; l++) {
+    if (avg[l] >= 90) {
+      answer += 'A';
+    } else if (avg[l] >= 80) {
+      answer += 'B';
+    } else if (avg[l] >= 70) {
+      answer += 'C';
+    } else if (avg[l] >= 50) {
+      answer += 'D';
+    } else {
+      answer += 'F';
+    }
+  }
   console.log(avg);
-  console.log(arr);
+
   return answer;
 }
 
