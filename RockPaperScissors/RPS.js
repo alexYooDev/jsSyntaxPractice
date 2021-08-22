8f9d6d9c8fbde3bbd615bf9a70538319111e9bf9
fm247 <hiyoo95@gmail.com>
2021-08-23 00:23:26 +0900

    RPS game UPDATE: my hands image section added

--- RockPaperScissors/RPS.js
+++ RockPaperScissors/RPS.js
@@ -5,6 +5,7 @@ const $rock = document.querySelector('#rock');
 const $paper = document.querySelector('#paper');
 const $scissors = document.querySelector('#scissors');
 const $score = document.querySelector('#score');
+const $myHand = document.querySelector('#myHand');
 
 // 서버 개발에서는 root경로(/)를 사용하는 경우가 많으나,
 // 서버를 사용하지 않는 경우엔 상대 경로 (./)를 사용
@@ -40,14 +41,18 @@ let intervalId = setInterval(shiftHands, 50);
 
 // clickBtn을 n번 호출하면, 1~n번 인터벌으로 id가 부여 -> 최종적으로는 n번이 id에 저장
 // 다음 버튼을 클릭하면 clearInterval은 n번 인터벌만 취소 : 1~n-1 인터벌이 멈추지 않는다.
-const clickBtn = () => {
+const clickBtn = (event) => {
   // 클릭하자 마자 실행
+  let myChoice = event.target.value;
+  console.log(myChoice);
   clearInterval(intervalId);
   $rock.removeEventListener('click', clickBtn);
   $paper.removeEventListener('click', clickBtn);
   $scissors.removeEventListener('click', clickBtn);
+  $myHand.style.background = `url(${IMG_URL}) ${rspXcoord[myChoice]}px 0 / auto 200px`;
   setTimeout(() => {
     // 클릭 1초 후에 실행
+    $myHand.style.background = '';
     clearInterval(intervalId);
     $rock.addEventListener('click', clickBtn);
     $paper.addEventListener('click', clickBtn);
